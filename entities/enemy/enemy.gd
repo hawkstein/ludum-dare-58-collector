3f2cd88c8bc0f50b8a4248ff6c7ab53b75053076
Liam Hawks <68058931+hawkstein@users.noreply.github.com>
2025-10-04 14:00:18 +0100

    add enemy move to target

--- entities/enemy/enemy.gd
+++ entities/enemy/enemy.gd
@@ -1,1 +1,20 @@
 extends Node2D
+
+var tower_positiom: Vector2 = Vector2(472, 281) # Hardcoded tower position
+var speed: float = 30.0
+var target_distance:float = 50.0
+var is_moving: bool = true
+var is_attacking: bool = false
+
+func _process(delta):
+	if not is_moving:
+		return
+	
+	var direction = (tower_positiom - global_position).normalized()
+	var distance_to_tower = global_position.distance_to(tower_positiom)
+	
+	if distance_to_tower < target_distance:
+		is_moving = false
+		is_attacking = true
+	else:
+		global_position += direction * speed * delta
